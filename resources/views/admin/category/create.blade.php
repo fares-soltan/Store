4211be190b69e4cd00d6ac610e23dc7ab6da4772
Fares <88165859+faresll@users.noreply.github.com>
2022-10-07 23:13:15 +0200

    category added done

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -17,22 +17,27 @@
                             <div class="col-md-6 mb-3">
                                 <label>Name</label>
                                 <input type="text" name="name" class="form-control">
+                                @error('name')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label>Slug</label>
                                 <input type="text" name="slug" class="form-control">
+                                @error('slug')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <label>description</label>
                                 <textarea name="description" class="form-control" rows="3"></textarea>
+                                @error('description')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label>Image</label>
                                 <input type="file" name="image" class="form-control">
+                                @error('image')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label>Status</label><br/>
                                 <input type="checkbox" name="status" style="margin-top: 16px;">
+
                             </div>
                             <div class="col-md-12 p-4">
                                 <h5 class="text-muted">SEO Tags</h5>
@@ -40,14 +45,17 @@
                             <div class="col-md-6 mb-3">
                                 <label>Meta Title</label>
                                 <input type="text" name="meta_title" class="form-control">
+                                @error('meta_title')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-6 mb-3">
                                 <label>Meta Keyword</label>
                                 <input type="text" name="meta_keyword" class="form-control">
+                                @error('meta_keyword')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <label>Meta Description</label>
                                 <textarea type="text" name="meta_description" class="form-control" rows="3"></textarea>
+                                @error('meta_description')<small class="text-danger">{{$message}}</small>@enderror
                             </div>
                             <div class="col-md-12 mb-3">
                                 <button type="submit" class="btn btn-primary btn-sm float-end">Save</button>
