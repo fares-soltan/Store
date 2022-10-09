9e2f9d6cba246a222107eab9814b4e0af7ca2432
Fares <88165859+faresll@users.noreply.github.com>
2022-10-10 01:48:12 +0200

    create livewire brands with view

---
+++ resources/views/livewire/admin/brand/index.blade.php
@@ -0,0 +1,31 @@
+<div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Brands List</h4>
+                    <a href="#">Add Brand</a>
+                </div>
+                <div class="card-body">
+                    <table class="table table-striped table-bordered">
+                        <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Name</th>
+                            <th>Slug</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
