5aa14c3322cd72b4bf88e1fe54ca9fefc29f9ceb
Fares <88165859+faresll@users.noreply.github.com>
2022-10-10 20:53:18 +0200

    create ProductController.php (index to return view) (create to return view and data $brands ,$categories)

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductFormRequest;
+use App\Models\Brand;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -10,5 +13,15 @@ class ProductController extends Controller
     public function index()
     {
         return view('admin.products.index');
+    }
+
+    public function create()
+    {
+        $brands = Brand::all();
+        $categories=Category::all();
+        return view('admin.products.create', compact('categories','brands'));
+    }
+    public function store(ProductFormRequest $request){
+        $request->validated();
     }
 }
